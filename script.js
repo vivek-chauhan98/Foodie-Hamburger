225d04e24d8a84428c8e3ad580c86051aada2375
Vivek Chauhan <vivekchauhan3597@gmail.com>
2024-11-12 14:14:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,68 +1,36 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu')
-const openMenuBox = document.querySelector('.open-menu-box')
+const sideMenu = document.querySelector('.side-menu')
 const closeTab = document.querySelector('.close-tab')
-const openMenuListItems = document.querySelector('open-menu-lists li')
 const goTop = document.querySelector('.go-top')
-const one = document.querySelectorAll('.one')
-const two = document.querySelectorAll('.two')
-const three = document.querySelectorAll('.three')
-const four = document.querySelectorAll('.four')
-const five = document.querySelectorAll('.five')
-const menuOne = document.querySelector('.menu-top')
-const menuTwo = document.querySelector('.menu-whopper')
-const menuThree = document.querySelector('.menu-stunner')
-const menuFour = document.querySelector('.menu-newfoddie')
-const menuFive= document.querySelector('.menu-deal')
-
-
+const mainContainer = document.querySelector('.main-container')
+const logo = document.querySelector('.logo')
+const menuLists = document.querySelectorAll('.side-menu a')
 
 
 hamburgerMenu.addEventListener('click', () => {    
     hamburgerMenu.style.display = 'none'
-    openMenuBox.style.display = 'flex'  
+    sideMenu.style.right = '24px'    
 })
 
-closeTab.addEventListener('click', () => {
-    openMenuBox.style.display = 'none'
+closeTab.addEventListener('click', () => {    
+    sideMenu.style.right = '-340px' 
     hamburgerMenu.style.display = 'flex'
 })
 
 goTop.addEventListener('click', () => {
-    window.scrollTo(0,0)
+    mainContainer.scrollTo(0,0)
 })
 
-one.forEach((el) => {
-    el.addEventListener('click', () => {
-        let y = menuOne.offsetTop
-        window.scrollTo(0,y)
-    })
+logo.addEventListener('click', () => {
+    mainContainer.scrollTo(0,0)
 })
 
-two.forEach((el) => {
+menuLists.forEach((el) => {
     el.addEventListener('click', () => {
-        let y = menuTwo.offsetTop
-        window.scrollTo(0,y)
-    })
-})
-
-three.forEach((el) => {
-    el.addEventListener('click', () => {
-        let y = menuThree.offsetTop
-        window.scrollTo(0,y)
-    })
-})
-
-four.forEach((el) => {
-    el.addEventListener('click', () => {
-        let y = menuFour.offsetTop
-        window.scrollTo(0,y)
-    })
-})
-
-five.forEach((el) => {
-    el.addEventListener('click', () => {
-        let y = menuFive.offsetTop
-        window.scrollTo(0,y)
+        setTimeout(() => {
+            sideMenu.style.right = '-340px' 
+            hamburgerMenu.style.display = 'flex'
+        }, 700)        
     })
 })
 
